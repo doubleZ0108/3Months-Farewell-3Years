b1291dd96835e98ad686a4b6f86bee5bd9e3bafb
double Z <doubleZ0108@163.com>
2020-09-03 21:53:09 +0800

    :alembic:farewell LinkList

--- Farewell-DataStructure/src/main.cpp
+++ Farewell-DataStructure/src/main.cpp
@@ -1,39 +1,17 @@
 #include <iostream>
 #include "LinkList/LinkList.h"
+#include "Queue/PascalTriangle.h"
+#include "Search/BinarySearch.h"
+#include "Sort/BinaryInsertSort.h"
+#include "Sort/ShellSort.h"
 
 int main() {
-    /**
-     * craete
-     */
-    LinkList* linkList = new LinkList(2);
 
-    /**
-     * traverse
-     */
-    linkList->init();
-    linkList->output();
-
-    /**
-     * insert
-     */
-    Node* node = new Node;
-    cout << "请输入新节点的信息: ";
-    cin >> node;
-    linkList->insert_before(1, *node);
-    linkList->insert_before(1, *node);
-
-    /**
-     * delete
-     */
-    Data data(2000, 1);
-    linkList->remove(data);
-
-    linkList->output();
-
-    /**
-     * destroy
-     */
-    delete linkList;
+//    testLinkList();
+//    testPascalTriangle();
+//    testBinarySearch();
+//    testBinaryInsertSort();
+    testShellSort();
 
     return 0;
 }
